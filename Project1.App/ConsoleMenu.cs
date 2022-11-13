9282800b1b21c776ce14f12cb3771d9c8113449b
AkhilBh97 <akhil416@revature.net>
2022-11-12 21:06:03 -0800

    Added ConsoleMenu.cs to git solution, since it was ignored at first

---
+++ Project1.App/ConsoleMenu.cs
@@ -0,0 +1,49 @@
+﻿using Project1.Logic;
+using Project1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1.App
+{
+    //
+    public class ConsoleMenu
+    {
+        //Fields
+        IRepository repo;
+
+        //Constructors
+        public ConsoleMenu() { }
+
+        //Methods
+        //Start the console menu, displays the first few options
+        public void Run() { 
+            //Option to Register
+
+            //Option to log in as an Employee
+
+            //Option to log in as a Manager
+        }
+
+        //Now for the different menus based on user's input
+        //User wants to register a new account
+        private void RegisterMenu()
+        {
+            //Have user enter an email
+
+
+            //Check that the email is not in use
+
+
+            //Have the user enter a password
+
+
+            //Have the user enter Y or N if they are a manager or not
+
+            
+            //
+        }
+    }
+}
